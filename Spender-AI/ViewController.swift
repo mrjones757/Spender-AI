ce666f8d9186dfbfe03d8c00ebf53a44b12e2d32
Stephen Jones <mrjones757@gmail.com>
2023-01-16 02:07:30 -0500

    Initial Commit

---
+++ Spender-AI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spender-AI
+//
+//  Created by Stephen Jones on 1/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
